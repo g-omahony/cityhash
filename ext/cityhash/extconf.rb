601709182d831ed3ac36e8c927334cb53c44f7b6
Ger O'Mahony <gomahony@kitmanlabs.com>
2025-10-15 18:03:43 +0100

    Fix for macOS Sequoia build

--- ext/cityhash/extconf.rb
+++ ext/cityhash/extconf.rb
@@ -3,6 +3,9 @@ require 'mkmf'
 cxx = ENV['CXX'] || with_config('CXX')
 RbConfig::MAKEFILE_CONFIG['CXX'] = cxx if cxx
 
+# Add Homebrew include/lib paths if available
+dir_config('cityhash', '/opt/homebrew/include', '/opt/homebrew/lib')
+
 %w{g O3 Wall}.each do |flag|
   flag = "-#{flag}"
   $CPPFLAGS += " #{flag}" unless $CPPFLAGS.split.include? flag
